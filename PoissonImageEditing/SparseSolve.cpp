bd82a07ad210475b9952f34bc8232b3d34abe1f8
zhengxuping <xupingzheng@gmail.com>
2017-07-22 15:32:13 +0800

    增加黑白模式

--- PoissonImageEditing/SparseSolve.cpp
+++ PoissonImageEditing/SparseSolve.cpp
@@ -162,7 +162,8 @@ void makeIndex(const cv::Mat& mask, cv::Mat& index, int& numElems)
     numElems = count;
 }
 
-void draw(const std::vector<cv::Point>& contour, const cv::Size& imageSize, cv::Rect& extendRect, cv::Mat& mask)
+void draw(const std::vector<cv::Point>& contour, const cv::Size& imageSize, 
+    cv::Rect& extendRect, cv::Mat& mask)
 {
     cv::Rect contourRect = cv::boundingRect(contour);
 
@@ -189,7 +190,8 @@ void draw(const std::vector<cv::Point>& contour, const cv::Size& imageSize, cv::
     mask.setTo(0);
     std::vector<std::vector<cv::Point> > contours(1);
     contours[0] = contour;
-    cv::drawContours(mask, contours, -1, cv::Scalar(255), -1, 8, cv::noArray(), 0, cv::Point(-left, -top));
+    cv::drawContours(mask, contours, -1, cv::Scalar(255), -1, 8, 
+        cv::noArray(), 0, cv::Point(-left, -top));
 }
 
 void draw(const std::vector<cv::Point>& contour, cv::Size& size, cv::Mat& mask)
@@ -208,7 +210,8 @@ void getEquation(const cv::Mat& src, const cv::Mat& dst,
     CV_Assert(src.data && dst.data && mask.data && index.data);
     CV_Assert((src.type() == CV_8UC1) && (dst.type() == CV_8UC1) &&
         (mask.type() == CV_8UC1) && (index.type() == CV_32SC1));
-    CV_Assert((src.size() == dst.size()) && (src.size() == mask.size()) && (src.size() == index.size()));
+    CV_Assert((src.size() == dst.size()) && (src.size() == mask.size()) && 
+        (src.size() == index.size()));
     
     int rows = src.rows, cols = src.cols;
     A.create(count, 8);
@@ -344,10 +347,27 @@ void copy(const cv::Mat& val, const cv::Mat& mask, const cv::Mat& index, cv::Mat
     }
 }
 
+/*!
+  The basic Poisson image editing function.
+  Source image, mask image and destination image should have the same size.
+  Source image's content inside the mask's non zero region will be blended into 
+  the destination image, using Possion image editing algorithm.
+  \param[in] src      Source image, should be of type CV_8UC1 or CV_8UC3.
+  \param[in] mask     Source image's mask. Source image's content inside the mask's 
+                      non zero region will be blended into the destination image.
+                      The mask's non zero region should not include the boundaries,
+                      i.e., left most and right most columns and top most and 
+                      bottom most columns, otherwise the result may be incorrect.
+  \param[in,out] dst  Destination image, should be the same cv::Mat::type() as
+                      the source image.
+  \param[in] mixGrad  True to apply mixing gradient operation.
+*/
 void PoissonImageEdit(const cv::Mat& src, const cv::Mat& mask, cv::Mat& dst, bool mixGrad = false)
 {
     CV_Assert(src.data && mask.data && dst.data);
     CV_Assert(src.size() == mask.size() && mask.size() == dst.size());
+    CV_Assert(src.type() == CV_8UC1 || src.type() == CV_8UC3);
+    CV_Assert(dst.type() == src.type());
     CV_Assert(mask.type() == CV_8UC1);
 
     cv::Mat index;
@@ -361,7 +381,8 @@ void PoissonImageEdit(const cv::Mat& src, const cv::Mat& mask, cv::Mat& dst, boo
         getEquation(src, dst, mask, index, numElems, A, b, x, mixGrad);
         std::vector<int> diagPos;
         A.calcDiagonalElementsPositions(diagPos);
-        solve(A.data, &A.count[0], &diagPos[0], (double*)b.data, (double*)x.data, A.rows, A.maxCols, 10000, 0.01);
+        solve(A.data, &A.count[0], &diagPos[0], (double*)b.data, (double*)x.data, 
+            A.rows, A.maxCols, 10000, 0.01);
         copy(x, mask, index, dst);
     }
     else if (src.type() == CV_8UC3)
@@ -377,27 +398,48 @@ void PoissonImageEdit(const cv::Mat& src, const cv::Mat& mask, cv::Mat& dst, boo
 
         for (int i = 0; i < 3; i++)
         {
-            getEquation(srcROISplit[i], dstROISplit[i], mask, index, numElems, A, b, x, mixGrad);
+            getEquation(srcROISplit[i], dstROISplit[i], mask, index, numElems, 
+                A, b, x, mixGrad);
             std::vector<int> diagPos;
             A.calcDiagonalElementsPositions(diagPos);
-            solve(A.data, &A.count[0], &diagPos[0], (double*)b.data, (double*)x.data, A.rows, A.maxCols, 10000, 0.01);
+            solve(A.data, &A.count[0], &diagPos[0], (double*)b.data, (double*)x.data, 
+                A.rows, A.maxCols, 10000, 0.01);
             copy(x, mask, index, dstROISplit[i]);
         }
         cv::merge(dstROISplit, 3, dst);
     }
 }
 
+/*!
+  Overloaded Poisson image editing function.
+  Source image, mask image and destination image should have the same size.
+  Source image's content inside the mask's non zero region will be blended into 
+  the destination image, using Possion image editing algorithm.
+  \param[in] src          Source image, should be of type CV_8UC1 or CV_8UC3.
+  \param[in] srcContour   A contour indicating the region of interest in the 
+                          source image. Pixels inside the region will be blended to 
+                          the destination image. The region enclosed by the contour 
+                          should not contain pixels on the border of the source image.
+  \param[in] ofsSrcToDst  The offset of the source image's region of interest in 
+                          the destination image. Pixel (x, y) in the region of interest in 
+                          the source image will be blended in (x, y) + ofsSrcToDst 
+                          in the destination image. You should make sure 
+                          that the destination image's region of interest locates totally inside 
+                          the destionation image excluding the border pixels. 
+  \param[in,out] dst      Destination image, should be the same cv::Mat::type() as
+                          the source image.
+  \param[in] mixGrad      True to apply mixing gradient operation.
+*/
 void PoissonImageEdit(const cv::Mat& src, const std::vector<cv::Point>& srcContour,
     cv::Point ofsSrcToDst, cv::Mat& dst, bool mixGrad = false)
 {
-    cv::Mat mask, index;
-    SparseMat A;
-    cv::Mat b, x;
-    int numElems;
+    CV_Assert(src.data && (src.type() == CV_8UC1 || src.type() == CV_8UC3));
+    CV_Assert(srcContour.size() >= 3);
+    CV_Assert(dst.data && dst.type() == src.type());
 
+    cv::Mat mask;
     cv::Rect srcRect;
     draw(srcContour, src.size(), srcRect, mask);
-    makeIndex(mask, index, numElems);
 
     cv::Mat srcROI = src(srcRect);
     cv::Mat dstROI = dst(srcRect + ofsSrcToDst);
@@ -449,17 +491,35 @@ cv::Rect getNonZeroBoundingRectExtendOnePixel(const cv::Mat& mask)
     return cv::Rect(left - 1, top - 1, right - left + 3, bottom - top + 3);
 }
 
+/*!
+  Overloaded Poisson image editing function.
+  Source image, mask image and destination image should have the same size.
+  Source image's content inside the mask's non zero region will be blended into 
+  the destination image, using Possion image editing algorithm.
+  \param[in] src          Source image, should be of type CV_8UC1 or CV_8UC3.
+  \param[in] srcMask      Source image's mask indicating the region of interest in the 
+                          source image. Pixels inside the region will be blended to 
+                          the destination image. The region enclosed by the contour 
+                          should not contain pixels on the border of the source image.
+  \param[in] ofsSrcToDst  The offset of the source image's region of interest in 
+                          the destination image. Pixel (x, y) in the region of interest in 
+                          the source image will be blended in (x, y) + ofsSrcToDst 
+                          in the destination image. You should make sure 
+                          that the destination image's region of interest locates totally inside 
+                          the destionation image excluding the border pixels. 
+  \param[in,out] dst      Destination image, should be the same cv::Mat::type() as
+                          the source image.
+  \param[in] mixGrad      True to apply mixing gradient operation.
+*/
 void PoissonImageEdit(const cv::Mat& src, const cv::Mat& srcMask,
     cv::Point ofsSrcToDst, cv::Mat& dst, bool mixGrad = false)
 {
-    cv::Mat mask, index;
-    SparseMat A;
-    cv::Mat b, x;
-    int numElems;
+    CV_Assert(src.data && (src.type() == CV_8UC1 || src.type() == CV_8UC3));
+    CV_Assert(srcMask.data && srcMask.type() == CV_8UC1);
+    CV_Assert(dst.data && dst.type() == src.type());
 
     cv::Rect srcRect = getNonZeroBoundingRectExtendOnePixel(srcMask);
-    mask = srcMask(srcRect);
-    makeIndex(mask, index, numElems);
+    cv::Mat mask = srcMask(srcRect);
 
     cv::Mat srcROI = src(srcRect);
     cv::Mat dstROI = dst(srcRect + ofsSrcToDst);
@@ -467,11 +527,18 @@ void PoissonImageEdit(const cv::Mat& src, const cv::Mat& srcMask,
     return;
 }
 
+#define GRAY 1
+
 void main()
 {
     {
+        // Images from http://www.ctralie.com/Teaching/PoissonImageEditing/
         cv::Mat src = cv::imread("GreatWhiteShark.jpg");
         cv::Mat dst = cv::imread("beach.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
 
         std::vector<cv::Point> contour(4);
         contour[0] = cv::Point(380, 300) - cv::Point(320, 230);
@@ -487,9 +554,14 @@ void main()
     }
 
     {
+        // Eye photo and hand photo from 
+        // https://en.wikipedia.org/wiki/Gradient-domain_image_processing
         cv::Mat src = cv::imread("220px-EyePhoto.jpg");
-        //cv::Mat dst = cv::imread("1074px-HandPhoto.jpg");
-        cv::Mat dst = cv::imread("1024px-Big_Tree_with_Red_Sky_in_the_Winter_Night.jpg");
+        cv::Mat dst = cv::imread("1074px-HandPhoto.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
 
         std::vector<cv::Point> srcContour(4);
         srcContour[0] = cv::Point(1, 1);
@@ -504,15 +576,43 @@ void main()
         cv::waitKey(0);
     }
 
-    // image sources http://cs.brown.edu/courses/csci1950-g/results/proj2/pdoran/
+    {
+        // Eye photo from 
+        // https://en.wikipedia.org/wiki/Gradient-domain_image_processing
+        cv::Mat src = cv::imread("220px-EyePhoto.jpg");
+        cv::Mat dst = cv::imread("1024px-Big_Tree_with_Red_Sky_in_the_Winter_Night.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
 
+        std::vector<cv::Point> srcContour(4);
+        srcContour[0] = cv::Point(1, 1);
+        srcContour[1] = cv::Point(218, 1);
+        srcContour[2] = cv::Point(218, 130);
+        srcContour[3] = cv::Point(1, 130);
+        cv::Point ofsSrcToDst(570, 300);
+
+        PoissonImageEdit(src, srcContour, ofsSrcToDst, dst, true);
+        cv::imshow("src", src);
+        cv::imshow("dst", dst);
+        cv::waitKey(0);
+    }
+
+    // following image sources
+    //  http://cs.brown.edu/courses/csci1950-g/results/proj2/pdoran/
     {
         cv::Mat src = cv::imread("src_img01.jpg");
         cv::Mat srcMask = cv::imread("mask_img01.jpg", cv::IMREAD_GRAYSCALE);
         cv::Mat dst = cv::imread("tar_img01.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
         cv::Point ofsSrcToDst(200, 200);
         cv::threshold(srcMask, srcMask, 128, 255, cv::THRESH_BINARY);
         PoissonImageEdit(src, srcMask, ofsSrcToDst, dst, true);
+        cv::imshow("src", src);
         cv::imshow("dst", dst);
         cv::waitKey(0);
     }
@@ -521,9 +621,14 @@ void main()
         cv::Mat src = cv::imread("src_img02.jpg");
         cv::Mat srcMask = cv::imread("mask_img02.jpg", cv::IMREAD_GRAYSCALE);
         cv::Mat dst = cv::imread("tar_img02.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
         cv::Point ofsSrcToDst(20, 200);
         cv::threshold(srcMask, srcMask, 128, 255, cv::THRESH_BINARY);
         PoissonImageEdit(src, srcMask, ofsSrcToDst, dst, true);
+        cv::imshow("src", src);
         cv::imshow("dst", dst);
         cv::waitKey(0);
     }
@@ -532,9 +637,14 @@ void main()
         cv::Mat src = cv::imread("src_img03.jpg");
         cv::Mat srcMask = cv::imread("mask_img03.jpg", cv::IMREAD_GRAYSCALE);
         cv::Mat dst = cv::imread("tar_img03.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
         cv::Point ofsSrcToDst(20, 20);
         cv::threshold(srcMask, srcMask, 128, 255, cv::THRESH_BINARY);
         PoissonImageEdit(src, srcMask, ofsSrcToDst, dst, true);
+        cv::imshow("src", src);
         cv::imshow("dst", dst);
         cv::waitKey(0);
     }
@@ -543,9 +653,14 @@ void main()
         cv::Mat src = cv::imread("src_img04.jpg");
         cv::Mat srcMask = cv::imread("mask_img04.jpg", cv::IMREAD_GRAYSCALE);
         cv::Mat dst = cv::imread("tar_img04.jpg");
+#if GRAY
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+#endif
         cv::Point ofsSrcToDst(20, 20);
         cv::threshold(srcMask, srcMask, 128, 255, cv::THRESH_BINARY);
         PoissonImageEdit(src, srcMask, ofsSrcToDst, dst, true);
+        cv::imshow("src", src);
         cv::imshow("dst", dst);
         cv::waitKey(0);
     }
